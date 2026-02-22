772e16a1f72a22a6314eb31cb6887bf00e35bc5f
datastatz <155395474+datastatz@users.noreply.github.com>
2026-02-22 23:32:16 +0100

    add stuff

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <complex.h>
 
-int main(){
-    printf("Hello World\n");
-    return 0;
+int main(void){
+    int x = 17;
+    int y = 2;
+
+    float f = (float)x / y;
+
+    printf("%f\n", f);
+
+    _Static_assert(__STDC_VERSION__ >= 201112L, "You need at least C11!");
+
 }
